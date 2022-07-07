d96e349dea1bcebc8ccea30a4cd4f6ac8333ab13
EugeneBrave <eugene.linn@gmail.com>
2022-07-07 19:05:22 +0800

    start

---
+++ txt.py
@@ -0,0 +1,14 @@
+USE_LINK = "使用連結驗證"
+RECEIVE_COIN = "今日簽到獲得"
+SHOPEE_REWARD = "蝦幣獎勵"
+TOO_MUCH_TRY = "您已達到今日驗證次數上限。"
+COIN_RECEIVED = "明天再回來領取"
+WRONG_PASSWORDS = [
+    "你的帳號或密碼不正確，請再試一次",
+    "登入失敗，請稍後再試或使用其他登入方法",
+    "您輸入的帳號或密碼不正確，若遇到困難，請重設您的密碼。",
+]
+PLAY_PUZZLE = "點擊以重新載入頁面"
+EMAIL_AUTH = "透過電子郵件連結驗證"
+FAILURE = "很抱歉，您的身份驗證已遭到拒絕。"
+FAILURE2 = "很抱歉，您的操作次數過多，請稍後再試。"
